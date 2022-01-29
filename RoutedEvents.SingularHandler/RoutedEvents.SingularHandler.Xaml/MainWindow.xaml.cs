21ed9c73f4b74ba274be21731b7a4eeb061fdb4d
Stanislav Kazakov <skazakov.workmail@gmail.com>
2022-01-30 01:10:55 +0300

    Add RoutedEvents.SingularHandler.Xaml project

---
+++ RoutedEvents.SingularHandler/RoutedEvents.SingularHandler.Xaml/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+
+namespace RoutedEvents.SingularHandler.Xaml
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement frameworkElement = e.Source as FrameworkElement;
+
+            switch (frameworkElement.Name)
+            {
+                case "Yes":
+                    OnYesButtonClick();
+                    break;
+                case "No":
+                    OnNoButtonClick();
+                    break;
+                case "Cancel":
+                    OnCancelButtonClick();
+                    break;
+                default:
+                    OnUnknownButtonClick();
+                    break;
+            }
+
+            e.Handled = true;
+        }
+
+        private void OnYesButtonClick()
+        {
+            MessageBox.Show("Yes");
+        }
+
+        private void OnNoButtonClick()
+        {
+            MessageBox.Show("No");
+        }
+
+        private void OnCancelButtonClick()
+        {
+            MessageBox.Show("Cancel");
+        }
+
+        private void OnUnknownButtonClick()
+        {
+            MessageBox.Show("Unknown");
+        }
+    }
+}
