3ec6218beb6775e1f3a5670c926024668e24e1bf
Stanislav Kazakov <skazakov.workmail@gmail.com>
2022-01-30 14:22:06 +0300

    Add RoutedEvents.CustomRoutedEvent.Xaml project

---
+++ RoutedEvents.CustomRoutedEvent/RoutedEvents.CustomRoutedEvent.Xaml/SendBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RoutedEvents.CustomRoutedEvent.Xaml
+{
+    public partial class SendBox : UserControl
+    {
+        // Определение идентификатора маршрутизируемого события.
+        public static readonly RoutedEvent SendEvent =
+            EventManager.RegisterRoutedEvent(   // Регистрирует событие в системе событий.
+                "Send",                         // Имя события.
+                RoutingStrategy.Bubble,         // Стратегия маршрутизации.
+                typeof(RoutedEventHandler),     // Тип делегата события.
+                typeof(SendBox));               // Тип владельца события.
+
+        // CLR оболочка события.
+        public event RoutedEventHandler Send
+        {
+            add => AddHandler(SendEvent, value);
+            remove => RemoveHandler(SendEvent, value);
+        }
+
+        // Метод, инициирующий событие.
+        protected virtual void OnSend()
+        {
+            RoutedEventArgs args = new RoutedEventArgs(SendEvent);
+            RaiseEvent(args);
+        }
+
+        public SendBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Message => _inputTextBox.Text;
+
+        private void OnSendButtonClick(object sender, RoutedEventArgs e)
+        {
+            OnSend();
+            _inputTextBox.Clear();
+        }
+    }
+}
