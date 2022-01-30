5d99372a84e24de282f2270d4083f8003c7cd434
Stanislav Kazakov <skazakov.workmail@gmail.com>
2022-01-30 20:47:26 +0300

    Add RoutedEvents.CustomRoutedEvent.CSharp project

---
+++ RoutedEvents.CustomRoutedEvent/RoutedEvents.CustomRoutedEvent.CSharp/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RoutedEvents.CustomRoutedEvent.CSharp
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ListBox _outputListBox = new ListBox();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeProperties();
+            InitializeWindowContent();
+        }
+
+        private void InitializeProperties()
+        {
+            Title = "Custom Routed Event";
+            ResizeMode = ResizeMode.CanMinimize;
+            SizeToContent = SizeToContent.WidthAndHeight;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void InitializeWindowContent()
+        {
+            Grid grid = new Grid
+            {
+                Margin = new Thickness(1),
+                MinHeight = 576,
+                MinWidth = 384
+            };
+
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+
+            _outputListBox.Margin = new Thickness(1);
+
+            var virtualizingStackPanel =
+                new FrameworkElementFactory(typeof(VirtualizingStackPanel));
+            virtualizingStackPanel.SetValue(VerticalAlignmentProperty, VerticalAlignment.Bottom);
+
+            _outputListBox.ItemsPanel = new ItemsPanelTemplate
+            {
+                VisualTree = virtualizingStackPanel
+            };
+
+            grid.Children.Add(_outputListBox);
+            Grid.SetRow(_outputListBox, 0);
+
+            SendBox sendBox = new SendBox
+            {
+                Margin = new Thickness(1),
+                MinHeight = 24
+            };
+
+            sendBox.Send += OnSend;
+
+            grid.Children.Add(sendBox);
+            Grid.SetRow(sendBox, 1);
+
+            Content = grid;
+        }
+
+        private void OnSend(object sender, RoutedEventArgs e)
+        {
+            if (!(e.Source is SendBox sendBox) ||
+                string.IsNullOrWhiteSpace(sendBox.Message))
+            {
+                return;
+            }
+
+            _outputListBox.Items.Add(new ListBoxItem { Content = sendBox.Message });
+        }
+    }
+}
---
+++ RoutedEvents.CustomRoutedEvent/RoutedEvents.CustomRoutedEvent.CSharp/SendBox.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace RoutedEvents.CustomRoutedEvent.CSharp
+{
+    public class SendBox : UserControl
+    {
+        private readonly TextBox _inputTextBox = new TextBox();
+        private readonly Button _sendButton = new Button();
+
+        // Определение идентификатора маршрутизируемого события.
+        public static readonly RoutedEvent SendEvent =
+            EventManager.RegisterRoutedEvent(   // Регистрирует событие в системе событий.
+                "Send",                         // Имя события.
+                RoutingStrategy.Bubble,         // Стратегия маршрутизации.
+                typeof(RoutedEventHandler),     // Тип делегата события.
+                typeof(SendBox));               // Тип владельца события.
+
+        // CLR оболочка события.
+        public event RoutedEventHandler Send
+        {
+            add => AddHandler(SendEvent, value);
+            remove => RemoveHandler(SendEvent, value);
+        }
+
+        // Метод, инициирующий событие.
+        protected virtual void OnSend()
+        {
+            RoutedEventArgs args = new RoutedEventArgs(SendEvent);
+            RaiseEvent(args);
+        }
+
+        public SendBox()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Grid grid = new Grid { Margin = new Thickness(1) };
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+            _inputTextBox.VerticalContentAlignment = VerticalAlignment.Center;
+            _inputTextBox.Margin = new Thickness(1);
+            grid.Children.Add(_inputTextBox);
+            Grid.SetColumn(_inputTextBox, 0);
+
+            _sendButton.Content = "Send";
+            _sendButton.Padding = new Thickness(12, 3, 12, 3);
+            _sendButton.Margin = new Thickness(1);
+            _sendButton.IsDefault = true;
+            _sendButton.Click += OnSendButtonClick;
+
+            var binding = new Binding("Text.Length") { Source = _inputTextBox };
+            _sendButton.SetBinding(IsEnabledProperty, binding);
+            grid.Children.Add(_sendButton);
+            Grid.SetColumn(_sendButton, 1);
+
+            Border border = new Border
+            {
+                Child = grid,
+                Background = SystemColors.ControlBrush,
+                BorderBrush = SystemColors.ActiveBorderBrush,
+                BorderThickness = new Thickness(1)
+            };
+
+            Content = border;
+        }
+
+        public string Message => _inputTextBox.Text;
+
+        private void OnSendButtonClick(object sender, RoutedEventArgs e)
+        {
+            OnSend();
+            _inputTextBox.Clear();
+        }
+    }
+}
